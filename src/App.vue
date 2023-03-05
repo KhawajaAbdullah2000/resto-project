6bfbb43b983a9cfdea89b880493df36a1fd28282
unknown <k200385@nu.edu.pk>
2023-03-05 14:01:12 +0500

    second push

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Signup from './components/Signup.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+<Signup/>
+
+</template>
+
+<style scoped>
+
+</style>
