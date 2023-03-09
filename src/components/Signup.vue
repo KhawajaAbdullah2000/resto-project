3bd844762fce3693e19763b082e3da14124369ed
unknown <k200385@nu.edu.pk>
2023-03-09 13:05:36 +0500

    options api setup

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,10 +1,18 @@
 <template>
     <div>
-   <h2 class="signup">Signup</h2>
+   <h2 class="signup">Signup {{count}}</h2>
+     <button @click="count++">Count is: {{ count }}</button>
     </div>
 </template>
 
 <script>
-   
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
 </script>
+
 
